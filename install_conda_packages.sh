dac209d0c12a9968e8d43a0717dbb00e17b14716
Adam Labadorf <alabadorf@gmail.com>
2021-02-19 11:02:31 -0500

    additional software deps

--- install_conda_packages.sh
+++ install_conda_packages.sh
@@ -1,5 +1,5 @@
 conda install -y -c conda-forge mamba
 
-mamba install -y -c bioconda -c conda-forge python=3 samtools numpy
+mamba install -y -c bioconda -c conda-forge python=3 samtools numpy libcairo2
 
-pip install jupyterlab pandas matplotlib pysam
+pip install jupyterlab pandas matplotlib pysam drawSvg
